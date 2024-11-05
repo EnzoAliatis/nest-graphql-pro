c3f9f5a560dabedd8aa8012871ce7a48b31cbfbb
EnzoAliatis <enzo.aliatis@gmail.com>
2024-11-05 14:20:45 -0500

    section 7 end

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,47 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    // Configuracion basicas de GraphQL
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+
+    // Configuracion de GraphQL con Autenticacion
+    // GraphQLModule.forRootAsync({
+    //   driver: ApolloDriver,
+    //   imports: [AuthModule],
+    //   inject: [JwtService],
+    //   useFactory: async (jwtService: JwtService) => ({
+    //     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    //     playground: false,
+    //     plugins: [ApolloServerPluginLandingPageLocalDefault],
+    //     context({ req }) {
+    //       try {
+    //         const token = req.headers.authorization?.replace('Bearer ', '');
+    //         const payload = jwtService.verify(token);
+    //         if (!payload) {
+    //           throw new Error('Invalid token');
+    //         }
+    //       } catch (error) {
+    //         console.log(error);
+    //         throw new Error('No token provided');
+    //       }
+
+    //       // if (!token) {
+    //       //   throw new Error('No token provided');
+    //       // }
+    //     },
+    //   }),
+    // }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -23,12 +60,6 @@ import { AuthModule } from './auth/auth.module';
       database: process.env.POSTGRES_DB,
       synchronize: true,
       autoLoadEntities: true,
-    }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     ItemsModule,
     UsersModule,
